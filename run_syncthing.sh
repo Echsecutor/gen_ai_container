57519a6d94c4c5832577e71ccc9d5f5b39eb86a8
Sebastian Schmittner <sebastian.schmittner@eecc.de>
2025-06-11 09:45:22 +0200

    add separate variable to switch syncthing on/off back in

--- run_syncthing.sh
+++ run_syncthing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Syncthing if it is enabled
-if [ "$(which syncthing)" ] && [ -n "${SYNCTHING_GUI_ADDRESS}" ] && [ -n "${SYNCTHING_DIR}" ]; then
+if [ "${RUN_SYNCTHING}" = "true" ] && [ "$(which syncthing)" ] && [ -n "${SYNCTHING_GUI_ADDRESS}" ] && [ -n "${SYNCTHING_DIR}" ]; then
     echo "Starting Syncthing on ${SYNCTHING_GUI_ADDRESS} with home directory ${SYNCTHING_DIR}"
     nohup syncthing --gui-address=${SYNCTHING_GUI_ADDRESS} --home=${SYNCTHING_DIR} &
 else
